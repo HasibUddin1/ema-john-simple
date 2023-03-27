51a5387034cefd93079abfce9c7469e666fde625
IconicLuck <uhasib959@gmail.com>
2023-03-27 19:20:52 +0600

    added product details

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
-    const {name, img, price, quantity, seller} = props.product
+    const { name, img, price, ratings, seller } = props.product
     return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <h6 className='text-2xl font-semibold mt-3'>{name}</h6>
+        <div id='product' className='product rounded-lg relative'>
+            <img className='rounded-lg p-2' src={img} alt="" />
+            <div className='flex flex-col justify-between p-2'>
+                <h5 className='text-2xl font-bold mt-3'>{name}</h5>
+                <h6 className='text-xl font-bold mt-3 mb-5'>Price: ${price}</h6>
+                <p className='font-semibold mt-3 mb-3'>Manufacturer: {seller}</p>
+                <p className='font-semibold mt-3 mb-3'>Ratings: {ratings} Stars</p>
+            </div>
+            <button className='bg-[#FFE0B3] w-full font-semibold hover:bg-orange-400 ease-in-out duration-200 h-12 btn-cart absolute bottom-0'>Add to Cart</button>
         </div>
     );
 };
