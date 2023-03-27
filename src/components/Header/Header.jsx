ea0ced67dcf459eb1b4bfdcef816e9ed366df61f
IconicLuck <uhasib959@gmail.com>
2023-03-27 11:51:17 +0600

    added header part of the website

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
+        <div className='bg-slate-900 h-20 flex items-center justify-between p-10'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div className='text-white flex gap-5 anchors'>
+                <a className='font-semibold' href="/order">Order</a>
+                <a className='font-semibold' href="/review">Order Review</a>
+                <a className='font-semibold' href="/inventory">Manage Inventory</a>
+                <a className='font-semibold' href="/login">Login</a>
+            </div>
         </div>
     );
 };
