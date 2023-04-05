e2d0a2b63d101b9f3dd8c4db3b5cf683cf89a871
IconicLuck <uhasib959@gmail.com>
2023-04-05 20:06:37 +0600

    added some changes on added product designs

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ReviewItem = ({ product }) => {
     console.log(product)
     const { id, img, name, price, quantity } = product
     return (
-        <div className='border-2 p-2 rounded-xl review-item'>
+        <div className='border-2 p-2 rounded-xl review-item flex justify-between items-center'>
             <div className='flex gap-5 items-center'>
                 <img className='rounded-xl' src={img} alt="" />
                 <div>
@@ -14,6 +16,7 @@ const ReviewItem = ({ product }) => {
                     <h2>Quantity: <span className='text-yellow-500 font-bold'>{quantity}</span></h2>
                 </div>
             </div>
+            <button className='bg-red-300 hover:bg-red-400 ease-in-out duration-200 rounded-full btn-delete'><FontAwesomeIcon className='text-red-600' icon={faTrashAlt} /></button>
         </div>
     );
 };
